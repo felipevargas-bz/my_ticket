44b435655364b8f6d197d8737ec208d5a66c7374
Felipe Vargas <felipevargas.bz@gmail.com>
2023-05-22 17:19:02 -0500

    update static url

--- ticket_sales/ticket_sales/settings.py
+++ ticket_sales/ticket_sales/settings.py
@@ -153,7 +153,6 @@ REST_FRAMEWORK = {
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
